4a0725fa69e8306f30d3cef371a0676c6a6fa7c5
aw7855689 <89001381+aw7855689@users.noreply.github.com>
2021-08-26 06:53:01 +0000

    up

--- lib/btcbamRPC.d.ts
+++
@@ -1,9 +0,0 @@
-import RpcClient, { IConfig } from "btcbamd-rpc";
-import { Network } from "./Network";
-export default class BtcbamRPC {
-    rpc: RpcClient;
-    constructor(config?: IConfig);
-    generate(nblocks: number): Promise<any>;
-}
-export declare const rpcClient: BtcbamRPC;
-export declare function generateBlock(network: Network): Promise<void>;
--- lib/constants.d.ts
+++
@@ -1,5 +0,0 @@
-export declare enum NetworkNames {
-    MAINNET = "btcbam",
-    TESTNET = "btcbam_testnet",
-    REGTEST = "btcbam_regtest"
-}
